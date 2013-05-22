a42f9aace8b55d237c4d9e4229666043075de647
Artem-PC <kiler4747@gmail.com>
2013-05-22 23:43:03 +0300

    Init commit

---
+++ BDContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeLib.Net
+{
+	public class BdContext:DbContext
+	{
+		public BdContext()
+		{
+			
+		}
+
+		public DbSet<Autor> Autors { get; set; }
+		public DbSet<Book> Books { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<Sequence> Sequences { get; set; }
+	}
+}
---
+++ Model/Autor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeLib.Net
+{
+	public class Autor
+	{
+		[Key]
+		public int IdAutor { get; set; }
+
+		public string FirstName { get; set; }
+		public string MiddleName { get; set; }
+		public string LastName { get; set; }
+		public string NickName { get; set; }
+		public string Email { get; set; }
+		public string Homepage { get; set; }
+
+		public List<Book> Books { get; set; }
+	}
+
+	public class Book
+	{
+		[Key]
+		public int IdBook { get; set; }
+
+		public string Title { get; set; }
+		public string Title1 { get; set; }
+		public int FileSize { get; set; }
+		public int Year { get; set; }
+		public bool Deleted { get; set; }
+		public string Keywords { get; set; }
+		public DateTime Time { get; set; }
+		public string Lang { get; set; }
+		public string FileType { get; set; }
+		public DateTime Modified { get; set; }
+
+		public List<Autor> Autors { get; set; }
+		public List<Genre> Genres { get; set; }
+		public List<Sequence> Sequences { get; set; }
+	}
+
+	public class Sequence
+	{
+		[Key]
+		public int IdSequence { get; set; }
+
+		public string Name { get; set; }
+		public List<Book> Books { get; set; }
+	}
+
+	public class Genre
+	{
+		[Key]
+		public int IdGenre { get; set; }
+
+		public string GenreCode { get; set; }
+		public string Description { get; set; }
+		public string Meta { get; set; }
+
+		public List<Book> Books { get; set; }
+	}
+}
